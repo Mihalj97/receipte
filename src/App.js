66ccb5c444cfa5f45191cd996454f078b93a6135
Mihalj97 <mihalj.tandi5@gmail.com>
2022-02-26 18:02:21 +0100

    Taking data from API examples

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const APP_ID = '46adbe9e'
+  const APP_KEY = '243d842a57615982c4b567f18d96f476'
+
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    getRecipes()
+  }, []) 
+  
+
+  // Returns all data with "chicken"
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json()
+    console.log(data.hits);
+
+    // Another example of taking data from API
+    // fetch(tps://api.edamam.com)
+    // .then(response => {
+    //   response.json
+    // })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <form className='search-form'>
+          <input type="text" className='search-bar' />
+          <button
+            type="submit"
+            className='search-button'>
+            Search
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
 
